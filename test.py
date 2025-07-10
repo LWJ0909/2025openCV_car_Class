24fa3f757f28d270fe2315e80a3b65d1b581f3db
Won Jong <won0109091834@gmail.com>
2025-07-10 18:21:18 +0900

    test

--- test.py
+++ test.py
@@ -58,21 +58,22 @@
 #         continue
 #     print('%d번째 학생은 합격입니다.' % number)
 
-coffee = 5
+# coffee = 5
 
-while True:
-    money = int(input('돈을 넣어주세요: (커피한잔 300원)'))
-    if money == 300:
-        print('커피를 드립니다.')
-        coffee -= 1
-    elif money > 300:
-        print('커피를 드리고, 거스름돈 %d원을 드립니다.' % (money - 300))
-        coffee -= 1
-    else:
-        print('돈을 다시 돌려주고 커피를 드리지 않습니다.')
+# while True:
+#     money = int(input('돈을 넣어주세요: (커피한잔 300원)'))
+#     if money == 300:
+#         print('커피를 드립니다.')
+#         coffee -= 1
+#     elif money > 300:
+#         print('커피를 드리고, 거스름돈 %d원을 드립니다.' % (money - 300))
+#         coffee -= 1
+#     else:
+#         print('돈을 다시 돌려주고 커피를 드리지 않습니다.')
 
-    print('남은 커피의 양은 %d개 입니다.' % coffee)
+#     print('남은 커피의 양은 %d개 입니다.' % coffee)
 
-    if coffee == 0:
-        print('커피가 다 떨어졌습니다. 판매를 중지합니다.')
-        break
+#     if coffee == 0:
+#         print('커피가 다 떨어졌습니다. 판매를 중지합니다.')
+#         break
+
